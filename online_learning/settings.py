a5c5a2f960e9c7d3a77636a0a86c71a817c6a0d1
panjing1111 <pjpanj@126.com>
2020-08-01 12:21:02 +0800

    将4个应用都移动到apps目录下（apps下存放该项目的所有子应用）

--- online_learning/settings.py
+++ online_learning/settings.py
@@ -11,9 +11,12 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 import configparser
 import os
+import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
 passwd = configparser.ConfigParser()
 passwd.read(os.path.join(BASE_DIR, 'passwd.ini'))
 
