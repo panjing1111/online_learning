4eb8bcd7a9f09c14cf8862c1e77140e4932a43a2
panjing1111 <pjpanj@126.com>
2020-08-01 14:49:59 +0800

    1.将用户模型注册进xadmin, 并且配置后台显示的列、搜索列、筛选列；修复xadmin导出csv乱码；3.修复django2.0的indexError；4.增加用户模型中的str重载。

--- apps/users/models.py
+++ apps/users/models.py
@@ -12,9 +12,9 @@ class UserProfile(AbstractUser):
         ("female", "女")
     )
     # 昵称
-    nick_name = models.CharField(max_length=50, verbose_name=u"昵称", default="")
+    nick_name = models.CharField(max_length=50, verbose_name="昵称", default="")
     # 生日，可以为空
-    birthday = models.DateField(verbose_name=u"生日", null=True, blank=True)
+    birthday = models.DateField(verbose_name="生日", null=True, blank=True)
     # 性别 只能男或女
     gender = models.CharField(
         max_length=6,
@@ -25,12 +25,13 @@ class UserProfile(AbstractUser):
     # 地址
     address = models.CharField(max_length=100, verbose_name="地址", default="")
     # 电话
-    mobile = models.CharField(max_length=11, null=True, blank=True)
+    mobile = models.CharField(max_length=11, null=True, blank=True, verbose_name="电话")
     # 头像 默认使用default.png
     image = models.ImageField(
         upload_to="image/%Y/%m",
-        default=u"image/default.png",
-        max_length=100
+        default="image/default.png",
+        max_length=100,
+        verbose_name = "头像"
     )
 
     class Meta:
@@ -47,33 +48,41 @@ class UserProfile(AbstractUser):
 # 邮箱验证码model
 class EmailVerifyRecord(models.Model):
     SEND_CHOICES = (
-        ("register", u"注册"),
-        ("forget", u"找回密码")
+        ("register", "注册"),
+        ("forget", "找回密码")
     )
-    code = models.CharField(max_length=20, verbose_name=u"验证码")
+    code = models.CharField(max_length=20, verbose_name="验证码")
     # 未设置null = true blank = true 默认不可为空
-    email = models.EmailField(max_length=50, verbose_name=u"邮箱")
-    send_type = models.CharField(choices=SEND_CHOICES, max_length=10)
+    email = models.EmailField(max_length=50, verbose_name="邮箱")
+    send_type = models.CharField(choices=SEND_CHOICES, max_length=10, verbose_name="验证码类型")
     # 这里的now得去掉(),不去掉会根据编译时间。而不是根据实例化时间。
-    send_time = models.DateTimeField(default=datetime.now)
+    send_time = models.DateTimeField(default=datetime.now, verbose_name="发送时间")
 
     class Meta:
         verbose_name = "邮箱验证码"
         verbose_name_plural = verbose_name
 
+    # 重载str方法使后台不再直接显示object
+    def __str__(self):
+        return '{0}({1})'.format(self.code, self.email)
+
 
 # 轮播图model
 class Banner(models.Model):
-    title = models.CharField(max_length=100, verbose_name=u"标题")
+    title = models.CharField(max_length=100, verbose_name="标题")
     image = models.ImageField(
         upload_to="banner/%Y/%m",
-        verbose_name=u"轮播图",
+        verbose_name="轮播图",
         max_length=100)
-    url = models.URLField(max_length=200, verbose_name=u"访问地址")
+    url = models.URLField(max_length=200, verbose_name="访问地址")
     # 默认index很大靠后。想要靠前修改index值。
-    index = models.IntegerField(default=100, verbose_name=u"顺序")
-    add_time = models.DateTimeField(default=datetime.now, verbose_name=u"添加时间")
+    index = models.IntegerField(default=100, verbose_name="顺序")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
 
     class Meta:
-        verbose_name = u"轮播图"
+        verbose_name = "轮播图"
         verbose_name_plural = verbose_name
+
+    # 重载str方法使后台不再直接显示object
+    def __str__(self):
+        return '{0}({1})'.format(self.title, self.index)
